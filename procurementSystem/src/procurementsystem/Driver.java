8b3b5062d193dbb22da20ea62665567138d306f3
haider1122-pgc <70263008+haider1122-pgc@users.noreply.github.com>
2021-04-04 15:14:10 -0700

    login screen for admin

--- procurementSystem/src/procurementsystem/Driver.java
+++ procurementSystem/src/procurementsystem/Driver.java
@@ -16,6 +16,8 @@ public class Driver {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        WelcomeScreen s=new WelcomeScreen();
+        s.setVisible(true);
     }
     
 }
